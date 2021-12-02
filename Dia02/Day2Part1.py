f79fd4d09ac1aea90bc1e6cc2a9915f2c6bbc740
linchsa <mepicalapie@gmail.com>
2021-12-02 09:53:25 +0100

    commit changes

--- Dia02/Day2Part1.py
+++ Dia02/Day2Part1.py
@@ -1,9 +1,9 @@
 file = open('C:/Users/diego/OneDrive/Escritorio/VSCODE/ConcursoAoC2021/Dia02/input.txt')
 list = file.readlines()
-h=0
-f=0
+h=0 #depth
+f=0 #forward
 for i in list:
-    final = i.split(' ')
+    final = i.split(' ') #.split separates the elements of a list so as it creates a new list for each line ['word',number]
     if(final[0]=='up'): h-=int(final[1])
     elif(final[0]=='down'): h+=int(final[1])
     else: f+=int(final[1])
